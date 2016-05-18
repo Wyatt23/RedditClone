93f0389ec951b11be581a9c597c3cf5feca13b94
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-05-18 01:21:02 -0400

    Create a  fucntion that will add an object to the posts array

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ app.controller("MainCtrl", [
   "$scope",
   function($scope){
     $scope.test = "Hello World!";
-   $scope.posts = [
+    $scope.posts = [
      {title: "post 1", upvotes: 4},
      {title: "post 2", upvotes: 3},
      {title: "post 3", upvotes: 3},
      {title: "post 4", upvotes: 2},
      {title: "post 5", upvotes: 6}
-   ]
+    ]
+    $scope.addPost = function(){
+     $scope.posts.push({title: "A New Post!", upvotes: 0});
+    }
   }
 ]);
