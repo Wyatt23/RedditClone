a4758d70b183671051ef33f76d832231a82e50df
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-05-18 00:47:31 -0400

    added list of posts using ng-repeat

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var app = angular.module("bombsForBreakfast", []); 
+var app = angular.module("bombsForBreakfast", []);
 
 app.controller("MainCtrl", [
   "$scope",
   function($scope){
     $scope.test = "Hello World!"
   }
-
-])
+   $scope.posts = [
+     "post 1",
+     "post 2",
+     "post 3",
+     "post 4",
+     "post 5"
+   ];
+]);
